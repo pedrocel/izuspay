11a2c4342573b48728c4f29cf9eaaee12bc9d326
pedrocel <pedrinhonovais10@gmail.com>
2025-08-30 07:02:15 -0300

    asdad

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -553,7 +553,7 @@
 
                 const result = await response.json();
                 
-                if (response.ok && result.status === 'paid') {
+                if (response.ok && result.status !== 'paid') {
                     clearInterval(verificarInterval); // Para de verificar
                     
                     paymentStatusBox.innerHTML = `
